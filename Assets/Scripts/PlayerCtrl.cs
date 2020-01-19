2644997bb63c293e881ea85fd6c57eb52d9ad7d4
Petipois <developer@petipois.co.uk>
2020-01-19 08:35:05 +0000

    All files
    All the files that make up the 2D endless runner

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float speed = 1.4f;
+    Rigidbody2D playerR;
+    private void Awake()
+    {
+        playerR = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            playerR.velocity = Vector2.up * speed;
+        }
+    }
+}
